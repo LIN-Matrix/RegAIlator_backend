c8cca1de2ece691f0a8f181e064a9614ef26ff1a
Yichen Lin <2848327718@qq.com>
2024-11-20 00:48:53 +0800

    monitor

--- monitor.sh
+++ monitor.sh
@@ -4,13 +4,13 @@ while true; do
     # 启动 npm run dev，并将输出重定向到 out.log
     npm run dev > out.log 2>&1 &
     NPM_PID=$!
-    echo "已启动 npm run dev，进程ID为 $NPM_PID"
+    echo "已启动 npm run dev, 进程ID为 $NPM_PID"
 
     # 监控 out.log 文件的新内容
     tail -n0 -F out.log | while read LINE; do
-        echo "$LINE" | grep -q -E "IMAP connection (closed|ended)"
+        echo "$LINE" | grep -q -E "IMAP connection (closed|ended|error)"
         if [ $? -eq 0 ]; then
-            echo "检测到 IMAP connection closed 或 IMAP connection ended，正在重新启动 npm run dev..."
+            echo "检测到 IMAP connection closed 或 IMAP connection ended 或 IMAP connection error, 正在重新启动 npm run dev..."
             kill $NPM_PID
             break
         fi
@@ -18,6 +18,6 @@ while true; do
 
     # 等待 npm run dev 进程退出
     wait $NPM_PID
-    echo "npm run dev 已退出，1秒后重新启动..."
+    echo "npm run dev 已退出, 1秒后重新启动..."
     sleep 1
 done
