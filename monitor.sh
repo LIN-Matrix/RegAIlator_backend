b608d1c3eeae3a4aeae18b71869ac37acf914ba4
Yichen Lin <2848327718@qq.com>
2024-11-22 14:40:51 +0800

    monitor

--- monitor.sh
+++ monitor.sh
@@ -8,9 +8,17 @@ while true; do
 
     # 监控 out.log 文件的新内容
     tail -n0 -F out.log | while read LINE; do
+        # 检查日志内容中是否包含 IMAP connection closed、IMAP connection ended、IMAP connection error 或 app crashed
         echo "$LINE" | grep -q -E "IMAP connection (closed|ended|error)"
         if [ $? -eq 0 ]; then
             echo "检测到 IMAP connection closed 或 IMAP connection ended 或 IMAP connection error, 正在重新启动 npm run dev..."
+            kill $NPM_PID
+            break
+        fi
+
+        echo "$LINE" | grep -q "app crashed"
+        if [ $? -eq 0 ]; then
+            echo "检测到 app crashed，正在重新启动 npm run dev..."
             kill $NPM_PID
             break
         fi
